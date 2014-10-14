d74dc5605442a1081da97377d71d695bb5d0ab00
Michael Pinter <mpinter09@gmail.com>
2014-10-13 17:02:10 -0700

    Deleted old files

--- js/devtools.js
+++
@@ -1,4 +0,0 @@
-chrome.devtools.panels.create("Family Photos",
-                              "icon.png",
-                              "panel.html",
-                              function(panel) { console.log("hello from callback"); });
